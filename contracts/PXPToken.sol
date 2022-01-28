b1eb5fbb2d47260e6b65078b19ede1a91deb7e68
muzashi36 <vegabone36@gmail.com>
2022-01-28 10:14:35 +0700

    Add Token Contract

---
+++ contracts/PXPToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PXPToken is ERC20, Pausable, Ownable {
+
+    constructor() ERC20("Pirate X Pirate", "PXP") {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+    internal
+    whenNotPaused
+    override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
